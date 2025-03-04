ffcfd7a6b5a0ff3514e843feb500d1fbe6239297
Fabrice Brito <fabrice.brito@terradue.com>
2025-03-04 10:39:00 +0100

    fixes chunks

--- cloudless-mosaic/src/cloudless_mosaic/app.py
+++ cloudless-mosaic/src/cloudless_mosaic/app.py
@@ -133,7 +133,7 @@ def main(start_date:str, end_date:str, aoi: BBox, bands: RGBBands, collection: s
         stackstac.stack(
             items,
             assets=assets,
-            chunksize=chunksize=(1, 3, optimal_chunk_size, optimal_chunk_size),
+            chunksize=(1, 3, optimal_chunk_size, optimal_chunk_size),
             resolution=resolution,
             epsg=epsg
         )
